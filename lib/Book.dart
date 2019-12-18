b5d807f23e3a6b6492ce5c199ef1f0a0881ebf29
Nang Htet Oo Wai <nanghtetoowai@achromex.com>
2019-12-18 17:14:34 +0630

    initail commit

---
+++ lib/Book.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/FirstScreen.dart';
+
+class Book extends StatelessWidget {
+  final String name;
+  const Book (this.name);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightGreenAccent,
+        padding: EdgeInsets.all(10.0),
+        width: 100.0,
+        height: 100.0,
+      child: Column(
+        children: <Widget>[
+          Icon(Icons.add),
+          Text(name)
+        ],
+      )
+
+    );
+  }
+}
---
+++ lib/FirstScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/Book.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body:Container(
+        child:Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              Book("Product 1"),
+              Book("Product 2"),
+              Book("Product 3")
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
